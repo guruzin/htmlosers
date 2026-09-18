d0617f80e89c49e64d578cb55df38293a9fad720
Gustavo Rocha <gusttavoperes@gmail.com>
2026-09-18 02:14:31 -0300

    botao pra mudar tema escuro e claro

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,13 @@
+const botaoTema = document.querySelector("#botao-tema");
+const temaNoite = document.querySelector("#tema-noite");
+function darkMode(){
+    temaNoite.disabled = !temaNoite.disabled;
+    if (temaNoite.disabled) {
+botaoTema.textContent = "🌙";
+} else {
+botaoTema.textContent = "🌞";
+}
 
+}
+
+botaoTema.addEventListener("click", darkMode);
